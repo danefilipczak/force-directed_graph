5517f5030dad52acdaea3a33542ee8d5af910f6b
Dane Filipczak <dane@imaginationrock.com>
2017-09-26 09:46:02 -0400

    feat: added parser for external contributors json

--- contributors.js
+++ contributors.js
@@ -20,14 +20,22 @@
 
 contributors = [
 
-	{name: 'jahmal', connections: ['lydon']},
-	{name: 'lydon', connections: ['jahmal', 'shahira']},
-	{name: 'shahira', connections: ['lydon', 'jahmal']},
-	{name: 'dane', connections: ['lydon']},
-	{name: 'jahmal1', connections: ['lydon']},
-	{name: 'lydon1', connections: ['jahmal1', 'shahira']},
-	{name: 'shahira1', connections: ['lydon', 'jahmal1']},
-	{name: 'dane1', connections: ['jahmal']}
+	/*
+	Put new contributors here. Be careful about the syntax - 
+	'id' can be whatever you want, but it has to be consistent. 
+	'name' is what will show up on the screen. 
+	template:
+	{name: 'NAME WITHIN SINGLE QUOTES', id: 'ID WITHIN SINGLE QUOTES', connections: ['CONNECTIONS WITHIN SINGLE QUOTES', 'SEPERATED BY COMMAS']},
+	*/
+
+	{name: 'Jahmal B. Golden', id: 'jahmal', connections: ['lydon']},
+	{name: 'Lydon MacGregor', id: 'lydon', connections: ['jahmal', 'shahira']},
+	{name: 'Shahira Kudsi', id: 'shahira', connections: ['lydon', 'jahmal']},
+	{name: 'Dane Filipczak', id: 'dane', connections: ['lydon']}
+	// {id: 'jahmal1', connections: ['lydon']},
+	// {id: 'lydon1', connections: ['jahmal1', 'shahira']},
+	// {id: 'shahira1', connections: ['lydon', 'jahmal1']},
+	// {id: 'dane1', connections: ['jahmal']}
 
 
 ]
