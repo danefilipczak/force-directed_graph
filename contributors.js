a50280607e4cdc1765a2518835c63ca734e192fe
Dane Filipczak <dane@imaginationrock.com>
2017-09-26 10:29:05 -0400

    feat: added biography div

--- contributors.js
+++ contributors.js
@@ -1,23 +1,3 @@
-  // var Jahmal = new Node('jahmal', ['lydon']);
-  // var Lydon = new Node('lydon', ['jahmal', 'shahira']);
-  // var Shahira = new Node('shahira', ['lydon', 'jahmal']);
-  // var Dane = new Node('dane', ['lydon', 'jahmal']);
-
-
-
-
-// contributors = [
-
-// 	{name: 'jahmal', connections: ['lydon']},
-// 	{name: 'lydon', connections: ['jahmal', 'shahira']},
-// 	{name: 'shahira', connections: ['lydon', 'jahmal']},
-// 	{name: 'dane', connections: ['lydon', 'jahmal']},
-// 	{name: 'jahmal1', connections: ['lydon']}
-
-
-// ]
-
-
 contributors = [
 
 	/*
@@ -29,33 +9,21 @@ contributors = [
 	*/
 
 	{name: 'Jahmal B. Golden', id: 'jahmal', connections: ['lydon']},
-	{name: 'Lydon MacGregor', id: 'lydon', connections: ['jahmal', 'shahira']},
-	{name: 'Shahira Kudsi', id: 'shahira', connections: ['lydon', 'jahmal']},
-	{name: 'Dane Filipczak', id: 'dane', connections: ['lydon']}
-	// {id: 'jahmal1', connections: ['lydon']},
-	// {id: 'lydon1', connections: ['jahmal1', 'shahira']},
-	// {id: 'shahira1', connections: ['lydon', 'jahmal1']},
-	// {id: 'dane1', connections: ['jahmal']}
+	{name: 'Lydon MacGregor', id: 'lydon', connections: ['jahmal']},
+	{name: 'Heidi Reszies', id: 'heidi', connections: ['andrea']},
+	{name: 'Andrea Blancas Beltran', id: 'andrea', connections: ['heidi', 'mary-kim']},
+	{name: 'Mary-Kim Arnold', id: 'mary-kim', connections: ['andrea', 'sabra']},
+	{name: 'Sabra Embury', id: 'sabra', connections: ['sabra', 'hannah']},
+	{name: 'Hannah Lillith Assadi', id: 'hannah', connections: ['sabra']},
+
+
+	
+
+
+	
+
+	// {name: 'Shahira Kudsi', id: 'shahira', connections: ['lydon', 'jahmal']},
+	// {name: 'Dane Filipczak', id: 'dane', connections: ['lydon']}
 
 
 ]
-
-
-
-// contributors = [
-
-// 	{name: 'jahmal', connections: ['lydon']},
-// 	{name: 'lydon', connections: ['jahmal', 'shahira']},
-// 	{name: 'shahira', connections: ['lydon', 'jahmal']},
-// 	{name: 'dane', connections: ['lydon', 'jahmal']},
-// 	{name: 'jahmal1', connections: ['lydon']},
-// 	{name: 'lydon', connections: ['jahmal', 'shahira']},
-// 	{name: 'shahira', connections: ['lydon', 'jahmal']},
-// 	{name: 'dane', connections: ['lydon', 'jahmal']},
-// 	{name: 'jahmal1', connections: ['lydon']},
-// 	{name: 'lydon1', connections: ['jahmal1', 'shahira']},
-// 	{name: 'shahira1', connections: ['lydon', 'jahmal1']},
-// 	{name: 'dane1', connections: ['lydon1', 'jahmal']}
-
-
-// ]
